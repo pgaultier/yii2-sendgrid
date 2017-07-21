5611d0c6622dc464525963d98b8ecce819f4437e
Srikanth Vaddepati <srikanthvaddepati@gmail.com>
2017-07-21 17:49:50 +0530

    Typo fix

--- src/Mailer.php
+++ src/Mailer.php
@@ -107,7 +107,7 @@ class Mailer extends BaseMailer
                 $cid = isset($attachment['ContentID']) ? $attachment['ContentID'] : null;
                 $sendGridMail->addAttachment($attachment['File'], $attachment['Name'], $cid);
             }
-            foreach($message->getUniqueArguments as $key=>$value){
+            foreach($message->getUniqueArguments() as $key=>$value){
                 $sendGridMail->addUniqueArg($key,$value);
             }
 
